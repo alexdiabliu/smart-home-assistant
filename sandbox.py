b12b55eb3b78e5bb80c2ca49287464282492abbd
Alexander Diab-Liu <88401570+alexdiabliu@users.noreply.github.com>
2025-07-02 23:51:51 -0400

    Delete sandbox.py

--- sandbox.py
+++
@@ -1,8 +0,0 @@
-
-
-from utils import record_audio
-import os
-
-path = record_audio(duration=2)
-os.startfile(path)  # Open the recorded file with the default application
-print("Recorded file:", path)
